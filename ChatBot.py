aa3611d45284a2957ae85b0433d69f84aff62ddc
Data Magic <66531576+datamagic2020@users.noreply.github.com>
2021-07-01 14:54:29 +0530

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,32 @@
+
+#install packages
+# pip install "chatterbot==1.0.0"
+# pip install pytz
+
+# import required packages
+from chatterbot import ChatBot
+from chatterbot.conversation import Statement
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# create ChatBot
+chatBot = ChatBot('ChatBot')
+
+# create ChatBot trainer
+trainer = ChatterBotCorpusTrainer(chatBot)
+
+# Train ChatBot with English language corpus
+# you can train with different language
+# or with your custom .yam file
+trainer.train("chatterbot.corpus.english")
+
+# Greeting from chat bot
+print("Hi, I am ChatBot")
+
+# keep communicating with ChatBot
+while True:
+    # take user input/query
+    query = input(">>>")
+    # response from ChatBot
+    # put query on Statement format to avoid runtime alert messages
+    # Statement(text=query, search_text=query)
+    print(chatBot.get_response(Statement(text=query, search_text=query)))
